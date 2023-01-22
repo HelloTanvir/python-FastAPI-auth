95cf2c23519ee7f8768f0e94e14e0c81af5c9a56
HelloTanvir <tanvirhossain.butex@gmail.com>
2023-01-22 09:49:21 +0600

    initial code push

---
+++ app/api.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello World"}
