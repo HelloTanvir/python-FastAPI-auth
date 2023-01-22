f00e8c0afbae2efa3f82ab01a78286a1c375787c
HelloTanvir <tanvirhossain.butex@gmail.com>
2023-01-22 22:19:23 +0600

    taking mongo url from env variable

--- app/db/db.py
+++ app/db/db.py
@@ -1,8 +1,12 @@
+import os
+
 from pymongo import MongoClient
 
 from app.db.user_schema import user_schema
 
-client: MongoClient = MongoClient("mongodb://127.0.0.1:27017/")
+client: MongoClient = MongoClient(
+    os.environ.get("MONGO_URI", "mongodb://127.0.0.1:27017/")
+)
 db = client.FastAPI
 
 if "users" not in db.list_collection_names():
